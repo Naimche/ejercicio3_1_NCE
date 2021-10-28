e2b517db6774e703d0f72ef5ac96b6017d43bdfb
Naimche <91694129+Naimche@users.noreply.github.com>
2021-10-28 15:57:42 +0200

    finish

--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -1,3 +1,5 @@
+import java.lang.NullPointerException
+
 class Cuenta(val numCuenta: String, var saldo: Double = 0.0) {
     fun consultarSaldo(): String {
         return "Tienes $saldo€"
@@ -15,17 +17,22 @@ class Cuenta(val numCuenta: String, var saldo: Double = 0.0) {
 }
 
 class Persona(val DNI: String) {
-    var cuentas: Array<Cuenta> = arrayOf()
+    private var contador: Int = 0
+    var cuentas: Array<Cuenta?> = arrayOfNulls(3)
     fun incorporar(c: Cuenta) {
-        if (cuentas.size < 3) {
-            cuentas += c
-        }
+
+        cuentas.set(contador, c)
+        contador++
     }
 
     fun esMoroso(): Boolean {
         for (i in 0..2) {
-            if (cuentas[i]!!.saldo < 0) {
-                return true
+            try {
+                if (cuentas[i]!!.saldo < 0) {
+                    return true
+                }
+            } catch (e: NullPointerException) {
+                return false
             }
         }
         return false
@@ -41,17 +48,15 @@ class Persona(val DNI: String) {
 fun main() {
     var p1 = Persona("2132312h")
     var c1 = Cuenta("Num1", 0.0)
-    var c2 = Cuenta("Num2", 700.0)
+    var c2 = Cuenta("Num2", 750.0)
     p1.incorporar(c1)
     p1.incorporar(c2)
     c1.recibirAbonos(1100.0)
     c2.realizarPago(750.0)
-    try {
-        if (p1.esMoroso()) {
-            println("La persona con dni ${p1.DNI} moroso/a")
-        }
-    } catch (e: ArrayIndexOutOfBoundsException) {}
 
+    if (p1.esMoroso()) {
+        println("La persona con dni ${p1.DNI} moroso/a")
+    }
     p1.transferencia(c1, c2, 20.0)
     println("La cuenta ${c1.numCuenta} tiene actualmente ${c1.saldo}")
     println("La cuenta ${c2.numCuenta} tiene actualmente ${c2.saldo}")
