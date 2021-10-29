e66902e0a24f76b34cb0dd8250234b555cf54b87
Naimche <91694129+Naimche@users.noreply.github.com>
2021-10-29 20:26:07 +0200

    Size

--- src/main/kotlin/ejercicio5_10.kt
+++ src/main/kotlin/ejercicio5_10.kt
@@ -11,18 +11,18 @@ class Libros(val titulo: String, val autor: String, val numerodepaginas: Long, c
     }
 }
 
-class ConjuntoLibros(var nombre: String, var conjunto: Array<Libros?> = arrayOfNulls(10)) {
+class ConjuntoLibros(var nombre: String,val tamanio:Int,var conjunto: Array<Libros?> = arrayOfNulls(tamanio)) {
     var contador:Int = 0
     fun incorporar(c: Libros) {
-        if (contador < 10) {
+        if (contador < tamanio) {
             conjunto[contador] = c
             contador++
         }
     }
-}
+   }
 
 fun main() {
-    var conjunto= ConjuntoLibros("Ficcion")
+    var conjunto= ConjuntoLibros("Ficcion",3)
     var l1 = Libros("Diario de Ana Frank", "Ana Frank", 215L, 8)
     var l2 = Libros("adasd","adads", 1212L, 2)
     conjunto.incorporar(l1)
